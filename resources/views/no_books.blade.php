33e42c95151d282f9367a1548ef06a90bfa9a627
Jose Alonzo <lxgiwyll3345@gmail.com>
2023-06-30 04:00:29 +0800

    My Project

--- resources/views/no_books.blade.php
+++ resources/views/no_books.blade.php
@@ -4,6 +4,13 @@
 <head>
     <title>No Books Found</title>
     <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f8f8;
+            margin: 0;
+            padding: 0;
+        }
+
         .container {
             display: flex;
             justify-content: center;
@@ -11,15 +18,41 @@
             height: 100vh;
             text-align: center;
         }
+
         .message {
             font-size: 24px;
             font-weight: bold;
+            color: #333;
+        }
+
+        .emoji {
+            font-size: 50px;
+            margin-bottom: 20px;
+        }
+
+        .button {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .button:hover {
+            background-color: #45a049;
         }
     </style>
 </head>
 <body>
     <div class="container">
-        <p class="message">No books found.</p>
+        <div>
+            <span class="emoji">📚</span>
+            <p class="message">Oops! No books found.</p>
+            <p>Try searching with different criteria or add new books to the collection.</p>
+            <button class="button">Add New Book</button>
+        </div>
     </div>
 </body>
 </html>
